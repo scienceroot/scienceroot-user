fd093d3ef48c860e05abad046b48ab065d85b250
Sven Seemann <sven.seemann@scienceroot.com>
2018-01-20 14:34:20 +0100

    #1 - added terms and conditions component

---
+++ src/lib/src/terms/terms.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {ScrTermsAndConditionsComponent} from "./terms.component";
+
+@NgModule({
+  imports: [],
+  declarations: [
+    ScrTermsAndConditionsComponent
+  ],
+  exports: [
+    ScrTermsAndConditionsComponent
+  ],
+  providers: []
+})
+export class ScrTermsAndConditionsModule {
+
+}
