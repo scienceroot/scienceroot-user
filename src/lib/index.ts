9ea34b28c0783d264dfc3d78cb97a5e611fe1eef
Sven Seemann <sven.seemann@scienceroot.com>
2018-01-20 12:43:41 +0100

    exported routes module symbol

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,2 @@
-export * from './src/core/core.module';
+export * from './src/core/index';
 export * from './src/new/new.module';
---
+++ src/lib/src/core/index.ts
@@ -0,0 +1,2 @@
+export * from './routes.module';
+export * from './core.module';
