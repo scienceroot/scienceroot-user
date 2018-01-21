f1013ec210b431b8400152f578512bc90283d476
Sven Seemann <sven.seemann@scienceroot.com>
2018-01-21 11:44:34 +0100

    added mock of sciencerootFrontend header in demo app

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -3,28 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'demo-app',
   template: `
-    <div>
-      <h1>@scienceroot/user</h1>
-      <ul>
-        <li>
-          <a [routerLink]="['']">Home</a>
-        </li>
-      </ul>
+    <div class="menu">
+      <div class="logo-container">
+        <img src="" alt="" />
+      </div>
+      <div fxLayout="row">
+        <div fxFlex="">
 
+        </div>
+      </div>
     </div>
-
-    <div  fxLayout="row"
-          fxLayoutAlign="center">
-      <div  fxFlex="90%"
-            fxFlex.lt-md="100%"
-            fxFlex.lt-lg="90%"
-            fxFlex.xl="75%">
-        <router-outlet></router-outlet>
+    <div style="padding: 24px">
+      <div  fxLayout="row"
+            fxLayoutAlign="center">
+        <div  fxFlex="90%"
+              fxFlex.lt-md="100%"
+              fxFlex.lt-lg="90%"
+              fxFlex.xl="75%">
+          <router-outlet></router-outlet>
+        </div>
       </div>
     </div>
   `,
   styles: [`
-    
+    .menu {
+      padding: 8px 24px;
+      background-color: #B1B7C1;
+    }
+
+    .menu .logo-container {
+      height: 48px;
+      width: 48px;
+    }
   `]
 })
 export class AppComponent {
