217bf38e00ba2ade0876957671507a98dc998702
Sven Seemann <sven.seemann@scienceroot.com>
2018-02-04 16:31:00 +0100

    exported user symbols

--- src/lib/src/core/index.ts
+++ src/lib/src/core/index.ts
@@ -1,2 +1,11 @@
 export * from './routes.module';
 export * from './core.module';
+
+export * from './user.model';
+export * from './user.resolver';
+export * from './user.service';
+
+export * from './job-title.model';
+export * from './job.model';
+export * from './industry.model';
+export * from './interest.model';
