f6e7a36851242cde751747276db0437e6d891da3
Sven Seemann <sven.seemann@scienceroot.com>
2018-02-18 20:03:17 +0100

    save jwt on register success

--- src/lib/src/core/user.service.ts
+++ src/lib/src/core/user.service.ts
@@ -3,6 +3,8 @@ import {ScrUser} from "./user.model";
 import {HttpClient, HttpResponse} from "@angular/common/http";
 import "rxjs/add/operator/map";
 import {ScrActiveUserService} from "./active-user.service";
+import {ScrAuthenticationTokenStore} from "@scienceroot/security";
+
 
 const SCR_USER_BASE_PATH: string = 'https://api.scienceroots.com/users';
 const SCR_USER_RESGISTER_PATH: string = SCR_USER_BASE_PATH + '/register';
@@ -23,6 +25,7 @@ export class ScrUserService {
           let token = res.headers.get('Authorization');
           let user: ScrUser = ScrUser.fromObj(res.body);
 
+          ScrAuthenticationTokenStore.setToken(token);
           this.activeUserService.set(user);
 
           return user;
