7ba61b747907211389f8b3416132c2e6da4b5622
Sven Seemann <sven.seemann@scienceroot.com>
2018-02-24 17:29:24 +0100

    resolve user for edit form; add back button

--- src/lib/src/edit/edit.component.ts
+++ src/lib/src/edit/edit.component.ts
@@ -1,10 +1,24 @@
 import {Component} from "@angular/core";
+import {ScrUser} from "../core/user.model";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: '',
   template: `
-    <scr-user-edit-jobs>
+    <scr-user-edit-jobs [jobs]="user.jobs">
     </scr-user-edit-jobs>
+    <div class="actions">
+      <div  fxLayout="row"
+            fxLayoutAlign="end">
+        <div fxFlex="100px">
+          <a  mat-button=""
+              color="accent"
+              [routerLink]="['/user', user.uid, 'info']">
+            Back to Profile
+          </a>
+        </div>
+      </div>
+    </div>
   `,
   styles: [`
   
@@ -12,4 +26,9 @@ import {Component} from "@angular/core";
 })
 export class ScrUserEditComponent {
 
+  public user: ScrUser;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.user = this.activatedRoute.snapshot.data.user;
+  }
 }
--- src/lib/src/edit/edit.module.ts
+++ src/lib/src/edit/edit.module.ts
@@ -1,9 +1,15 @@
 import {NgModule} from "@angular/core";
 import {ScrUserEditComponent} from "./edit.component";
 import {ScrUserEditJobsModule} from "./jobs/jobs.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   imports: [
+    RouterModule,
+    FlexLayoutModule,
+    MatButtonModule,
     ScrUserEditJobsModule
   ],
   declarations: [
