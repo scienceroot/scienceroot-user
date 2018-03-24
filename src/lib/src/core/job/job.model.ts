784c2ad94d9ec365dcc93ffb898c056700f92f9c
Sven Seemann <sven.seemann@scienceroot.com>
2018-03-24 15:53:06 +0100

    added start/end to job

--- src/lib/src/core/job/job.model.ts
+++ src/lib/src/core/job/job.model.ts
@@ -12,14 +12,22 @@ export class ScrUserJob {
     return new ScrUserJob(
       obj.employer,
       obj.title,
-      ScrUserIndustry.fromObject(obj.industry)
+      ScrUserIndustry.fromObject(obj.industry),
+      obj.startMonth,
+      obj.startYear,
+      obj.endMonth,
+      obj.endYear
     );
   }
 
   constructor(
     public employer?: string,
     public title?: string,
-    public industry?: ScrUserIndustry
+    public industry?: ScrUserIndustry,
+    public startMonth?: number,
+    public startYear?: number,
+    public endMonth?: number,
+    public endYear?: number
   ) {
   }
 }
