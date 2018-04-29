dbdc56671c776633073fda5307937f7d6e6f8874
Sven Seemann <sven.seemann@scienceroot.com>
2018-04-29 12:47:32 +0200

    hide contact fields in profile when no data is present

--- src/lib/src/core/contact/contact.component.ts
+++ src/lib/src/core/contact/contact.component.ts
@@ -5,29 +5,43 @@ import {ScrUserContact} from './contact.model';
 @Component({
   selector: 'scr-user-contact',
   template: `
-    <div>
-      <div fxLayout="row">
-        <div fxFlex="64px">
-          <span class="mat-caption">Skype</span>
-        </div>
-        <div fxFlex="">
-          <a  class="mat-body-2"
-              [href]="skypeLink">
-            <span>{{ contact.skype }}</span>
-          </a>
-        </div>
+    <div class="mat-headline">
+        <span>
+          Contact
+        </span>
+    </div>
+    <ng-container *ngIf="!contact || (!contact.skype && !contact.phone && !contact.twitter)">
+      <div class="mat-body-1">
+        <span>No contact information available.</span>
       </div>
-      <div fxLayout="row">
-        <div fxFlex="64px">
-          <span class="mat-caption">Phone</span>
+    </ng-container>
+    <div *ngIf="!!contact">
+      <ng-container *ngIf="!!contact.skype">
+        <div fxLayout="row">
+          <div fxFlex="64px">
+            <span class="mat-caption">Skype</span>
+          </div>
+          <div fxFlex="">
+            <a  class="mat-body-2"
+                [href]="skypeLink">
+              <span>{{ contact.skype }}</span>
+            </a>
+          </div>
         </div>
-        <div fxFlex="">
-          <a  class="mat-body-2"
-              [href]="phoneLink">
-            {{ contact.phone }}
-          </a>
+      </ng-container>
+      <ng-container *ngIf="!!contact.phone">
+        <div fxLayout="row">
+          <div fxFlex="64px">
+            <span class="mat-caption">Phone</span>
+          </div>
+          <div fxFlex="">
+            <a  class="mat-body-2"
+                [href]="phoneLink">
+              {{ contact.phone }}
+            </a>
+          </div>
         </div>
-      </div>
+      </ng-container>
       <ng-container *ngIf="!!contact.twitter">
         <div fxLayout="row">
           <div fxFlex="64px">
