78700a7ded4dd98ad65a06f5868e54b2bc67992a
Sven Seemann <sven.seemann@scienceroot.com>
2018-06-24 10:51:00 +0200

    fixed api for isFollowedBy and follows; now receiving Fellowships from backend

--- user-lib/src/core/follower/follower.service.ts
+++ user-lib/src/core/follower/follower.service.ts
@@ -1,5 +1,6 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {map} from 'rxjs/operators';
 import {ScrUserStore} from '../../store/user.store';
 import {ScrUser} from '../user.model';
 
@@ -38,15 +39,27 @@ export class ScrUserFollowerService {
     const url: string = ScrUserStore.followedBy(userId);
 
     return this._httpClient.get(url)
-      .map(ScrUser.fromObjArr)
+      .pipe(
+        map((fellowships: ScrUserFellowship[]) => fellowships.map(fellowship => fellowship.follower)),
+        map(ScrUser.fromObjArr)
+      )
       .toPromise();
   }
 
   public follows(userId: string): Promise<ScrUser[]> {
     const url: string = ScrUserStore.follows(userId);
 
-    return this._httpClient.get(url)
-      .map(ScrUser.fromObjArr)
+    return this._httpClient.get<ScrUserFellowship[]>(url)
+      .pipe(
+        map((fellowships: ScrUserFellowship[]) => fellowships.map(fellowship => fellowship.followed)),
+        map(ScrUser.fromObjArr)
+      )
       .toPromise();
   }
+}
+
+export interface ScrUserFellowship {
+  id: string;
+  follower: ScrUser;
+  followed: ScrUser;
 }
